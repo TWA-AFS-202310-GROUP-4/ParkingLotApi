c3436bae513989f0267d442fef9f71b5367604ce
Dexin Wang <DWang36@slb.com>
2023-11-08 13:04:08 +0800

    test: test getweatcherforecast

---
+++ ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ParkingLotApiTest.Controllers
+{
+    public class WeatherForecastControllerTest
+    {
+
+        public HttpClient httpClient;
+        public WeatherForecastControllerTest()
+        {
+            WebApplicationFactory<Program> factory = new WebApplicationFactory<Program>();
+            httpClient = factory.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task Shaould_return_Ok_when_get_WeatherForecast()
+        {
+         var  responseMessage =  await httpClient.GetAsync("/WeatherForecast");
+         Assert.Equal(HttpStatusCode.OK,responseMessage.StatusCode);
+        }
+    }
+}
