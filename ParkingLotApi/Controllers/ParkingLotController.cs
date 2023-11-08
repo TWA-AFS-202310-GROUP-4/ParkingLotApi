c7e3fb3e52e9f0eac4d5de3cd120aef987af0b98
Ming Wang <MWang35@slb.com>
2023-11-08 14:19:15 +0800

    feat: partial implement parkinglot controller

---
+++ ParkingLotApi/Controllers/ParkingLotController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLotApi.Dtos;
+
+namespace ParkingLotApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingLotController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<ParkingLotDto>> AddParkingLotAsync([FromBody] ParkingLotDto data)
+        {
+            if (data.Capacity < 10)
+            {
+                return BadRequest();
+            }
+            return null;
+        }
+    }
+}
