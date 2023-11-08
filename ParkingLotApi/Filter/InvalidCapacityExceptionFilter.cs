6e5cd8349f9ddb0dc9925418270d63d98d15c3f5
Ming Wang <MWang35@slb.com>
2023-11-08 15:53:27 +0800

    refactor: refact exception message

---
+++ ParkingLotApi/Filter/InvalidCapacityExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ParkingLotApi.Exceptions;
+
+namespace ParkingLotApi.Filter
+{
+    public class InvalidCapacityExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order => int.MaxValue - 10;
+
+        public void OnActionExecuted(ActionExecutedContext context) // when controller method return
+        {
+            if (context.Exception is InvalidCapacityException)
+            {
+                context.Result = new BadRequestResult();
+                context.ExceptionHandled = true;
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context) // before controller method run
+        {
+        }
+    }
+}
