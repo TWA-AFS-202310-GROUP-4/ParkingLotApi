1f98e918032b761f1ce4824bf9cbf157a1f01074
Diana Jing <DJing@slb.com>
2023-11-08 13:19:10 +0800

    feat: add testbase file to refactor code.

--- ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
+++ ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace ParkingLotApiTest.Controllers
 {
-    public class WeatherForecastControllerTest
+    public class WeatherForecastControllerTest:TestBase
     {
-        private HttpClient _httpClient;
-        public WeatherForecastControllerTest()
+        public WeatherForecastControllerTest(WebApplicationFactory<Program> webApplicationFactory):base(webApplicationFactory)
         {
-            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>();
-            _httpClient = webApplicationFactory.CreateClient();
         }
 
         [Fact]
         public async Task Should_return_correctly_when_get_weather_forcast()
         {
             //given & when
+            HttpClient _httpClient = GetClient();
              HttpResponseMessage response= await _httpClient.GetAsync("/WeatherForecast");
 
             //then
