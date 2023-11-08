6639095e9c892f0d5daca1b2c3c0649c14752356
Diana Jing <DJing@slb.com>
2023-11-08 13:04:24 +0800

    test: add test case for weather forecast controller

---
+++ ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ParkingLotApiTest.Controllers
+{
+    public class WeatherForecastControllerTest
+    {
+        private HttpClient _httpClient;
+        public WeatherForecastControllerTest()
+        {
+            WebApplicationFactory<Program> webApplicationFactory = new WebApplicationFactory<Program>();
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_return_correctly_when_get_weather_forcast()
+        {
+            //given & when
+             HttpResponseMessage response= await _httpClient.GetAsync("/WeatherForecast");
+
+            //then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
