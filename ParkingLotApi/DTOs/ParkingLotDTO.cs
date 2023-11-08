003774ff40d136024e547c00f07547d90e3e70b0
Diana Jing <DJing@slb.com>
2023-11-08 16:54:21 +0800

    feat: redesgin DTO and models

--- ParkingLotApi/DTOs/ParkingLotDTO.cs
+++ ParkingLotApi/DTOs/ParkingLotDTO.cs
@@ -1,9 +1,21 @@
-﻿namespace ParkingLotApi.DTOs
+﻿using ParkingLotApi.Models;
+
+namespace ParkingLotApi.DTOs
 {
     public class ParkingLotDTO
     {
         public string Name { get; set; }
         public int Capacity { get; set; }
         public string Location { get; set; }
+
+        internal ParkingLot ToEntity()
+        {
+            return new ParkingLot()
+            {
+                Name = Name,
+                Capacity = Capacity,
+                Location = Location
+            };
+        }
     }
 }
