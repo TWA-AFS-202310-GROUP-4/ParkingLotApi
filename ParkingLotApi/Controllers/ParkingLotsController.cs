afc1f985591da9b8fe81015d16e7f40cf746348d
Xinwei <xlu15@slb.com>
2023-11-08 14:14:26 +0800

    feat: return bad request when capacity less than 10

---
+++ ParkingLotApi/Controllers/ParkingLotsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLotApi.Dtos;
+
+namespace ParkingLotApi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class ParkingLotsController : ControllerBase 
+{
+    [HttpPost]
+    public async Task<ActionResult<ParkingLotsRequestDto>> AddNewParkingLot([FromBody] ParkingLotsRequestDto parkingLotDto)
+    {
+        if (parkingLotDto.Capacity < 10)
+        {
+            return BadRequest();
+        }
+
+        return null;
+    }
+}
---
+++ ParkingLotApiTest/Controller/ParkingLotsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using ParkingLotApi.Dtos;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace ParkingLotApiTest.Controller;
+
+public class ParkingLotsControllerTest : TestBase
+{
+    public ParkingLotsControllerTest(WebApplicationFactory<Program> factory) : base(factory)
+    {
+
+    }
+
+    [Fact]
+    public async Task Should_return_bad_request_when_add_parking_lot_given_capacity_less_then_10()
+    {
+        //Given
+        HttpClient _httpClient = GetClient();
+        ParkingLotsRequestDto parkingLotCapacityLess10 = new ParkingLotsRequestDto() {Name = "Test Street", Location = "My Street", Capacity  = 8};
+
+        //When
+        HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/parkinglots", parkingLotCapacityLess10);
+
+        //then
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
